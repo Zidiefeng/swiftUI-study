3f72896ed56a5526409cd64b4bef2d622354c482
kaitan <sunkaitan123@163.com>
2021-11-18 16:26:30 -0500

    Module 5 app structure setup and local data parse

--- m5-learning-app/m5-learning-app/ViewModels/ContentModel.swift
+++ m5-learning-app/m5-learning-app/ViewModels/ContentModel.swift
@@ -8,6 +8,51 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+    @Published var modules: [Module] = [Module]()
     
+    var styleData : Data?
+    
+    init() {
+        self.getLocalData()
+    }
+    
+    func getLocalData(){
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            //use `.self` to pass the type
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            //assign modules to var
+            self.modules = modules
+        }
+        catch{
+            // log error
+            print("Could not parse local data")
+        }
+        
+        
+        // parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do{
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        catch{
+            // log error
+            print("Could not parse style data")
+        }
+        
+        
+        
+    }
 }
 
