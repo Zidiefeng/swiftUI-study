82bb55e0ddbf17aa016953cb48bcfe036630ea4a
kaitan <sunkaitan123@163.com>
2021-12-14 02:04:59 -0800

    map uikit view and coordinator(delegate)

--- m6-city-sights-app/m6-city-sights-app/Views/Home/Map/BusinessMap.swift
+++ m6-city-sights-app/m6-city-sights-app/Views/Home/Map/BusinessMap.swift
@@ -24,7 +24,7 @@ struct BusinessMap: UIViewRepresentable {
             if let lat = business.coordinates?.latitude, let long = business.coordinates?.longitude {
                 
                 // create a new annotation
-                var a = MKPointAnnotation()
+                let a = MKPointAnnotation()
                 
                 a.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
                 a.title = business.name ?? ""
@@ -38,6 +38,14 @@ struct BusinessMap: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         // create UIKit view
         let mapView = MKMapView()
+        
+        // create a delegate
+        // the map view will ask the delegate object like what annotation views to show
+        // when the user tap the annotation, the map view will notify the delegate object to see what to do next
+        // if there is an existing coordinator, use that coordinator
+        // if there is not an coordinator, create a new one
+        mapView.delegate = context.coordinator
+        
         
         // properties
         //make the user show up on the map
@@ -59,10 +67,55 @@ struct BusinessMap: UIViewRepresentable {
         // add annotations and zoom as well
         uiView.showAnnotations(self.locations, animated: true)
         
+        
+        
     }
     
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         // When this is not needed, we can clean up the UIKit view
-        uiView.removeAnnotations(uiView.annotations     )
+        uiView.removeAnnotations(uiView.annotations)
     }
+    
+    // MARK: coordinator
+    
+    // get automatically called when we need an instance of coordinator class
+    func makeCoordinator() ->  Coordinator {
+        return Coordinator()
+    }
+    
+    // declare this inside the view
+    // NSObject: to conform to MKMapViewDelegate, this need conform to NSObject (this is objective C stuff)
+    class Coordinator: NSObject, MKMapViewDelegate {
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            // Create an annotation view
+            
+            // if the annotation is the user blue dot, return nil
+            if annotation is MKUserLocation {
+                // check the type
+                return nil
+            }
+            
+            // check if there is a reusablw annotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseId)
+            
+            if annotationView == nil {
+                // create a new one
+                // assign a identifier to re-use annotation view even if it is out of the zoom
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseId)
+                
+                // return it
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                
+            }
+            else {
+                // we have a reusable one
+                annotationView!.annotation = annotation
+            }
+            
+            return annotationView
+        }
+    }
+    
+    
 }
